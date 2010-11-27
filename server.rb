054206fc958e6f120c778e7d90a2f6e2005a12f4
rhapsodhy <me@rhapsodhy.hu>
2010-11-27 15:42:08 +0100

    pimped up server: trollop for command line arguments, optional forking, pid file, kill running server

--- server.rb
+++ server.rb
@@ -1,38 +1,116 @@
 #!/usr/bin/env ruby
 #
-# controller library for the arduino.
+# Controller server for the arduino.
 #
-
-require 'socket'
+# Copyright (c) 2010 Peter Parkanyi <me@rhapsodhy.hu>.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
 require 'rubygems'
+require 'socket'
 require 'serialport'
+require 'trollop'
 
-#fork do
-  server = TCPServer.open 12355
+class Server
+  def listen
+    loop do
+      begin
+        sock = @server.accept
 
-  begin
-    serial = SerialPort.open ARGV[0], ARGV[1].to_i, 8, 1, SerialPort::NONE
-  rescue
-    print 'couldn\'t open serial port!'
-    exit
-  end
+        case sock.read 1
+        when 'w'
+          r = sock.read 4
+          (0...4).each { |i| puts r[i].to_s }
+          $stdout.flush
 
-  trap("INT") do
-    server.close
-  end
-
-  loop do
-    begin
-      sock = server.accept
-
-      serial.write sock.readpartial(4) if(sock.readpartial(1) == 'w')
-      serial.flush
-    rescue Errno::EBADF, IOError
-      break
-    ensure sock.close unless sock.nil?
+          @serial.write r
+          @serial.flush
+        end
+      rescue Errno::EBADF, IOError
+        break
+      ensure
+        sock.close unless sock.nil?
+      end
     end
   end
 
-  serial.close
-#end
+  def close
+    @server.close
+    @serial.close
+  end
+
+  def initialize
+    @server = TCPServer.open $opts[:port]
+  #  @serial = SerialPort.open $opts[:tty], $opts[:baud].to_i, 8, 1, SerialPort::NONE
+    @serial = File.new 'pina', 'w'
+  end
+end
+
+def start
+  trap("INT") do
+    @server.close
+  end
+
+  begin
+    @server = Server.new
+  rescue
+    print 'couldn\'t open serial port!'
+    exit 2
+  end
+end
+
+$opts = Trollop::options do
+  banner <<EOS
+Lowlight server application
+
+Usage: ./server.rb
+
+Available options:
+EOS
+
+  opt :tty,     'serial port to use', :short => 't', :default => '/dev/ttyUSB0', :type => :string
+  opt :baud,    'baud rate',          :short => 'b', :default => 9600,           :type => :int
+  opt :port,    'port to listen on',  :short => 'p', :default => 12355
+  opt :daemon,  'fork to background', :short => 'd'
+  opt :pidfile, 'the pidfile',        :short => 'P', :default => 'pid'
+  opt :kill,    'kill already running background process'
+end
+
+if $opts[:kill]
+  if File.exists? $opts[:pidfile]
+    Process.kill :SIGINT, File.open($opts[:pidfile], 'r') { |f| f.read }.to_i
+    File.delete $opts[:pidfile]
+
+    print 'server.rb: background process killed successfully!'
+    exit 0
+  else
+    print 'server.rb: no background process!'
+    exit 1
+  end
+end
+
+if $opts[:daemon]
+  fork do
+    File.open('pid', 'w') { |f| f.print Process.pid }
+    start.listen
+  end
+else
+  start.listen
+end
 
