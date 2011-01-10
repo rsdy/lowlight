8435e063435b064c8e89124fa2faa21348bd6d17
rhapsodhy <me@rhapsodhy.hu>
2011-01-10 01:10:53 +0100

    the controller can now set sample control; also some minor stuff

--- controller.rb
+++ controller.rb
@@ -2,7 +2,7 @@
 #
 # Controller application for the arduino.
 #
-# Copyright (c) 2010 Peter Parkanyi <me@rhapsodhy.hu>.
+# Copyright (c) 2010-2011 Peter Parkanyi <me@rhapsodhy.hu>.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -26,13 +26,30 @@ require 'rubygems'
 require 'socket'
 require 'Qt4'
 
+require 'lowlight'
+
+module SocketConnection
+  def socket &f
+    TCPSocket.open('localhost', 12355, &f)
+  end
+
+  def send_data data
+    socket { |sock| socket.write data }
+  end
+
+  def read_data chunks
+    socket { |sock| yield data while data = sock.read(chunks) }
+  end
+end
+
 class MainWindow < Qt::Dialog
+  include SocketConnection
+
   def create_picker led
     Qt::ColorDialog.new self do |d|
       d.setOption Qt::ColorDialog::NoButtons
       d.connect(SIGNAL('currentColorChanged(QColor)')) do |color|
-        str = ['w', led, color.red, color.green, color.blue].pack('AC*')
-        TCPSocket.open('localhost', 12355) { |sock| sock.write str }
+        send_data [Lowlight::WriteToArduino, led, color.red, color.green, color.blue].pack('AC*')
       end
     end
   end
@@ -46,14 +63,33 @@ class MainWindow < Qt::Dialog
     end
   end
 
+  def create_visualization_bar
+    Qt::GroupBox.new self do |w|
+      w.title = 'Visualization'
+      w.layout = Qt::VBoxLayout.new
+
+      checkbox = Qt::CheckBox.new 'Visualize sounds with LEDs' do |c|
+        c.connect(SIGNAL('toggled(bool)')) do |checked|
+          send_data case checked
+                      when true then Lowlight::SampleControl
+                      else Lowlight::DisableSampleControl
+                    end
+        end
+      end
+
+      w.layout.addWidget checkbox
+    end
+  end
+
   def initialize parent = nil
     super parent
 
     centralWidget = Qt::Widget.new self do |w|
       w.layout = Qt::VBoxLayout.new
       w.layout.addWidget create_picker_bar
+      w.layout.addWidget create_visualization_bar
       w.minimumWidth = 1045
-      w.minimumHeight = 375
+      w.minimumHeight = 475
     end
   end
 end
